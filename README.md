3549d75ee3f42ea92c0206c3b0ad1eedf488c499
Kamal <theoriginalkamal@gmail.com>
2020-04-15 01:14:28 +0530

    Udacity final project

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# mobile-flashcards
+# Flash cards - mobile app
+
+## Installation
+In order to run the app you need to have installed:
+1. Expo
+2. nodejs
+3. npm
+
+run
+```
+yarn install
+```
+```
+yarn start
+```
+
---
+++ utils/_DATA.js
@@ -0,0 +1,24 @@
+export const decks = {
+    React: {
+      title: 'React',
+      questions: [
+        {
+          question: 'Is React a library',
+          answer: 'True'
+        },
+        {
+          question: 'Is componentDidMount is good for aking ajax requests',
+          answer: 'true'
+        }
+      ]
+    },
+    JavaScript: {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: '== !== ====',
+          answer: 'true'
+        }
+      ]
+    }
+  }
